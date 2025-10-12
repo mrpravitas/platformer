39666393d79cb41d8e275e1b0385ee32ede122be
artem <homelesspravitas@gmail.com>
2025-10-12 17:18:07 +0600

    Updated graphics

--- Assets/Scripts/Components/SpriteSwitcher.cs
+++ Assets/Scripts/Components/SpriteSwitcher.cs
@@ -2,28 +2,36 @@ using UnityEngine;
 
 public class SpriteSwitcher : MonoBehaviour
 {
-    [SerializeField] private Sprite _sprite1;
-    [SerializeField] private Sprite _sprite2;
+    [SerializeField] private Sprite[] _sprites;
     [SerializeField] private float _switchInterval;
 
     private SpriteRenderer _spriteRenderer;
     private float _timer;
-    private bool _useSprite1 = true;
+    private int _currentIndex;
 
     private void Awake()
     {
         _spriteRenderer = GetComponent<SpriteRenderer>();
-        _spriteRenderer.sprite = _sprite1;
+
+        if (_spriteRenderer != null && _sprites.Length > 0)
+        {
+            _spriteRenderer.sprite = _sprites[0];
+        }
     }
 
     private void Update()
     {
+        if (_sprites == null || _sprites.Length == 0)
+        {
+            return;
+        }
+
         _timer += Time.deltaTime;
 
         if (_timer >= _switchInterval)
         {
-            _useSprite1 = !_useSprite1;
-            _spriteRenderer.sprite = _useSprite1 ? _sprite1 : _sprite2;
+            _currentIndex = (_currentIndex + 1) % _sprites.Length;
+            _spriteRenderer.sprite = _sprites[_currentIndex];
             _timer = 0f;
         }
     }
